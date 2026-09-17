2f49b23c0b1e913b68d632db6448a1820bdcc0ca
damian295 <damianvoglino@gmail.com>
2026-09-16 22:49:49 -0300

    Forzar carga del ajuste de Pase completo y Pintura del Río

--- reservas/v4.js
+++ reservas/v4.js
@@ -65,7 +65,7 @@ v5Script.onload=()=>{
     v7Script.src='v7.js?v=2';
     v7Script.onload=()=>{
       const v8Script=document.createElement('script');
-      v8Script.src='v8.js?v=2';
+      v8Script.src='v8.js?v=3';
       v8Script.onload=()=>{
         const liveScript=document.createElement('script');
         liveScript.src='live-integration.js?v=10';
